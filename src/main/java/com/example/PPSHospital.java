2b91283762015f64727f94be149c49cf46123282
knut <knutolav01@gmail.com>
2025-03-23 22:13:37 +0100

    Completed the runHospitalSimulation method in PPSHospital class

--- src/main/java/com/example/PPSHospital.java
+++ src/main/java/com/example/PPSHospital.java
@@ -17,8 +17,52 @@ public class PPSHospital implements HospitalSimulation {
 
     @Override
     public void runHospitalSimulation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'runHospitalSimulation'");
-    }
+        int n = patients.length;
+        int[] remainingTime = new int[n];
+        boolean[] isCompleted = new boolean[n];
+        int completed = 0;
 
+        // Copy burst times into remainingTime
+        for (int i = 0; i < n; i++) {
+            remainingTime[i] = patients[i].getBurstTime();
+        }
+
+        while (completed != n) {
+            int idx = -1;
+            int highestPriority = Integer.MAX_VALUE;
+
+            // Find prosess with the highest priority
+            for (int i = 0; i < n; i++) {
+                if (patients[i].getArrivalTime() <= currentTime &&
+                        !isCompleted[i] && patients[i].getPriority() < highestPriority &&
+                        remainingTime[i] > 0) {highestPriority = patients[i].getPriority();
+                    idx = i;
+                }
+            }
+
+            if (idx != -1) {
+                // Runs process for 1 unit
+                remainingTime[idx]--;
+                currentTime++;
+
+                // If the process is completed (the patient treated)
+                if (remainingTime[idx] == 0) {
+                    isCompleted[idx] = true;
+                    completed++;
+
+                    int turnaroundTime = currentTime - patients[idx].getArrivalTime();
+                    int waitingTime = turnaroundTime - patients[idx].getBurstTime();
+
+                    patients[idx].setTurnaroundTime(turnaroundTime);
+                    patients[idx].setWaitingTime(waitingTime);
+
+                    totalTurnaroundTime += turnaroundTime;
+                    totalWaitingTime += waitingTime;
+                }
+            } else {
+                // No process is available run for 1 unit
+                currentTime++;
+            }
+        }
+    }
 }
